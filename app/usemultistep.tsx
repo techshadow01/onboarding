f1be0ed911f6576226527a4ba5ea9df579d7dde3
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-03-10 01:30:22 +0530

    Update usemultistep.tsx

--- app/usemultistep.tsx
+++ app/usemultistep.tsx
@@ -2,14 +2,13 @@
 'use client';
 import { useState } from 'react';
 
-// Define the custom hook to manage step logic
+//  manage step logic
 export function usemultistep() {
     const [step, setStep] = useState<number>(1);
 
     // Function to increment the step
     function next(): void {
         setStep((prevStep) => prevStep + 1); // Increment the step
-        console.log(step); // You can remove this line in production
     }
 
     return {
